276b2d3cefedfa59d1c28debdfb789ae1789150c
ibrhmdurna <ibrhmdurna@gmail.com>
2019-03-21 01:41:49 +0300

    initial commit

--- app/src/main/java/com/ibrhmdurna/chatapp/Main/WriteActivity.java
+++ app/src/main/java/com/ibrhmdurna/chatapp/Main/WriteActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +15,8 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -24,11 +28,13 @@ import com.ibrhmdurna.chatapp.Utils.MessagesAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WriteActivity extends AppCompatActivity {
+public class WriteActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar toolbar;
     private TextView writeTitle;
     private LinearLayout searchInputLayout;
+    private EditText searchInput;
+    private ImageButton searchClearView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,15 +58,44 @@ public class WriteActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
+    private void inputProcess(){
+        searchInput.addTextChangedListener(inputWatcher);
+    }
+
+    private TextWatcher inputWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if(searchInput.getText().length() > 0){
+                searchClearView.setVisibility(View.VISIBLE);
+            }
+            else {
+                searchClearView.setVisibility(View.GONE);
+            }
+        }
+    };
+
     private void buildView(){
         toolbar = findViewById(R.id.write_toolbar);
         writeTitle = findViewById(R.id.write_title_view);
         searchInputLayout = findViewById(R.id.write_search_layout);
+        searchInput = findViewById(R.id.search_input);
+        searchClearView = findViewById(R.id.clear_search_btn);
     }
 
     private void toolsManagement() {
         Environment.toolbarProcess(this, R.id.write_toolbar);
         buildView();
+        inputProcess();
     }
 
     @Override
@@ -69,11 +104,11 @@ public class WriteActivity extends AppCompatActivity {
             Animation fadeIn = new AlphaAnimation(0, 1);
             fadeIn.setInterpolator(new DecelerateInterpolator());
             fadeIn.setDuration(500);
-            writeTitle.setAnimation(fadeIn);
             searchInputLayout.setAnimation(fadeIn);
             writeTitle.setVisibility(View.VISIBLE);
             searchInputLayout.setVisibility(View.VISIBLE);
             toolbar.getMenu().clear();
+            searchInput.requestFocus();
         }
         else if(searchInputLayout.getVisibility() == View.VISIBLE) {
             writeTitle.setVisibility(View.GONE);
@@ -90,5 +125,15 @@ public class WriteActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.write_menu, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.clear_search_btn:
+                searchInput.getText().clear();
+                searchClearView.setVisibility(View.GONE);
+                break;
+        }
     }
 }
