69f827100758633823f500418b63ab284d665541
ibrhmdurna <ibrhmdurna@gmail.com>
2019-03-23 20:30:30 +0300

    initial commit

---
+++ app/src/main/java/com/ibrhmdurna/chatapp/Database/FirebaseDB.java
@@ -0,0 +1,45 @@
+package com.ibrhmdurna.chatapp.Database;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseDB {
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+    private DatabaseReference database;
+
+    public FirebaseDB() {
+    }
+
+    public FirebaseAuth getAuth() {
+        if(this.auth == null)
+            this.auth = FirebaseAuth.getInstance();
+        return auth;
+    }
+
+    public void setAuth(FirebaseAuth auth) {
+        this.auth = auth;
+    }
+
+    public FirebaseUser getCurrentUser() {
+        if(this.currentUser == null)
+            this.currentUser = getAuth().getCurrentUser();
+        return currentUser;
+    }
+
+    public void setCurrentUser(FirebaseUser currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public DatabaseReference getDatabase() {
+        if(this.database == null)
+            this.database = FirebaseDatabase.getInstance().getReference();
+        return database;
+    }
+
+    public void setDatabase(DatabaseReference database) {
+        this.database = database;
+    }
+}
